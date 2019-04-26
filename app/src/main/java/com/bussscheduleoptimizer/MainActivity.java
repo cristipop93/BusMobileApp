4b5300d284188392afae5c183478adff4b475ed5
Cristi <pcristi1993@gmail.com>
2019-04-26 11:07:35 +0300

    Use input to predict

--- app/src/main/java/com/bussscheduleoptimizer/MainActivity.java
+++ app/src/main/java/com/bussscheduleoptimizer/MainActivity.java
@@ -57,22 +57,21 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                float prediction = doInference(Float.valueOf(idFrom.getText().toString()),
-//                        Float.valueOf(idTo.getText().toString()),
-//                        Float.valueOf(vehicleType.getText().toString()),
-//                        Float.valueOf(month.getText().toString()),
-//                        Float.valueOf(day.getText().toString()),
-//                        Float.valueOf(hour.getText().toString()),
-//                        Float.valueOf(minute.getText().toString()),
-//                        Float.valueOf(holiday.getText().toString()),
-//                        Float.valueOf(vacation.getText().toString()),
-//                        Float.valueOf(temperature.getText().toString()),
-//                        Float.valueOf(pType.getText().toString())
-//                );
-//                float prediction = doInference(0, 1, 0, 5, 3, 15, 10, 0, 0, 25, 0);
-//                secondsDelay.setText(Float.toString(prediction));
+                float prediction = doInference(Float.valueOf(idFrom.getText().toString()),
+                        Float.valueOf(idTo.getText().toString()),
+                        Float.valueOf(vehicleType.getText().toString()),
+                        Float.valueOf(month.getText().toString()),
+                        Float.valueOf(day.getText().toString()),
+                        Float.valueOf(hour.getText().toString()),
+                        Float.valueOf(minute.getText().toString()),
+                        Float.valueOf(holiday.getText().toString()),
+                        Float.valueOf(vacation.getText().toString()),
+                        Float.valueOf(temperature.getText().toString()),
+                        Float.valueOf(pType.getText().toString())
+                );
+                secondsDelay.setText(Float.toString(prediction));
 
-                sendToPythonServer();
+//                sendToPythonServer();
             }
         });
     }
@@ -83,26 +82,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public float doInference(float idFrom, float idTo, float vehicleType, float month, float day, float hour, float minute, float holiday, float vacation, float temperature, float pType) {
-        float[] inputVals = new float[11];
-        inputVals[0] = day;
-        inputVals[1] = holiday;
-        inputVals[2] = hour;
-        inputVals[3] = idFrom;
-        inputVals[4] = idTo;
-        inputVals[5] = minute;
-        inputVals[6] = month;
-        inputVals[7] = pType;
-        inputVals[8] = temperature;
-        inputVals[9] = vacation;
-        inputVals[10] = vehicleType;
+        Object[] inputVals = new Object[11];
+        inputVals[0] = new float[]{day};
+        inputVals[1] = new float[]{holiday};
+        inputVals[2] = new float[]{hour};
+        inputVals[3] = new float[]{idFrom};
+        inputVals[4] = new float[]{idTo};
+        inputVals[5] = new float[]{minute};
+        inputVals[6] = new float[]{month};
+        inputVals[7] = new float[]{pType};
+        inputVals[8] = new float[]{temperature};
+        inputVals[9] = new float[]{vacation};
+        inputVals[10] = new float[]{vehicleType};
+
+        float[][] outputVal = new float[1][1];
+        Map<Integer, Object> outputs = new HashMap();
+        outputs.put(0, outputVal);
 
 //        Map<Integer, Object> outputVal = new HashMap();
 //        outputVal.put(0, new Object());
-        float[] outputVal = new float[1];
-        tflite.run(inputVals, outputVal);
+        tflite.runForMultipleInputsOutputs(inputVals, outputs);
 
 //        return (Float) outputVal.get(0);
-        return outputVal[0];
+        return outputVal[0][0];
     }
 
     private MappedByteBuffer loadModelFile() throws IOException {
